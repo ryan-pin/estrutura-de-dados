c99818674d138f8ea5c95261b624686b7e73738c
unknown <ryan.piinheiro@hotmail.com>
2025-09-29 10:38:22 -0300

    feat: adiciona deque com lista simplesmente ligada

---
+++ Deque/SimpleLinkedListDeque.java
@@ -0,0 +1,137 @@
+public class SimpleLinkedListDeque<T> {
+    
+    private class Node {
+        T data;
+        Node next;
+        
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    
+    private Node head;
+    private Node tail;
+    private int tamanho;
+    
+    public SimpleLinkedListDeque() {
+        this.head = null;
+        this.tail = null;
+        this.tamanho = 0;
+    }
+    
+    // Operação O(1)
+    public void inserirInicio(T item) {
+        Node newNode = new Node(item);
+        
+        if (estaVazio()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        tamanho++;
+    }
+    
+    // Operação O(1) 
+    public void inserirFim(T item) {
+        Node newNode = new Node(item);
+        
+        if (estaVazio()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+        tamanho++;
+    }
+    
+    // Operação O(1) 
+    public T removerInicio() {
+        if (estaVazio()) {
+            throw new RuntimeException("Deque está vazio");
+        }
+        
+        T data = head.data;
+        head = head.next;
+        
+        if (head == null) { 
+            tail = null;
+        }
+        
+        tamanho--;
+        return data;
+    }
+    
+    // Operação O(n)
+    public T removerFim() {
+        if (estaVazio()) {
+            throw new RuntimeException("Deque está vazio");
+        }
+        
+        T data = tail.data;
+        
+        if (tamanho == 1) {
+            head = tail = null;
+        } else {
+            Node current = head;
+            while (current.next != tail) {
+                current = current.next;
+            }
+            current.next = null;
+            tail = current;
+        }
+        
+        tamanho--;
+        return data;
+    }
+    
+    // Operação O(1)
+    public T primeiro() {
+        if (estaVazio()) {
+            throw new RuntimeException("Deque está vazio");
+        }
+        return head.data;
+    }
+    
+    // Operação O(1)
+    public T ultimo() {
+        if (estaVazio()) {
+            throw new RuntimeException("Deque está vazio");
+        }
+        return tail.data;
+    }
+    
+    // Operação O(1)
+    public boolean estaVazio() {
+        return tamanho == 0;
+    }
+    
+    // Operação O(1)
+    public int tamanho() {
+        return tamanho;
+    }
+      
+    // Teste
+    public static void main(String[] args) {
+        SimpleLinkedListDeque<Integer> deque = new SimpleLinkedListDeque<>();
+        
+        deque.inserirInicio(10);
+        deque.inserirFim(20);
+        deque.inserirInicio(5);
+        deque.inserirFim(30);
+        System.out.println(deque.tamanho());
+        
+        System.out.println(deque.primeiro());
+        System.out.println(deque.ultimo());
+        
+        System.out.println(deque.removerInicio());
+        
+        System.out.println(deque.removerFim());
+        
+        System.out.println(deque.tamanho());
+
+        // O problema esta na remoção no fim, que é O(n)
+        // Já que é necessário percorrer a lista para chegar ao penultimo nó e atualizar o ponteiro
+    }
+}
