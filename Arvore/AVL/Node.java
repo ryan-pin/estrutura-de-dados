8e6b198991fe2430469d833f723133631719c810
Ryan <ryan.piinheiro@hotmail.com>
2026-04-13 18:51:48 -0300

    feat: adiciona node a avl

---
+++ Arvore/AVL/Node.java
@@ -0,0 +1,41 @@
+public class Node {
+    public Object value =  (Integer) null;
+    public Node filhoD = null;
+    public Node filhoE =  null;
+    public Node Pai =  null;
+    public int FB = 0;
+
+    public Object getValue() {
+        return value;
+    }
+    public void setValue(Object value) {
+        this.value = value;
+    }
+    public Node getFilhoD() {
+        return filhoD;
+    }
+    public void setFilhoD(Node filhoD) {
+        this.filhoD = filhoD;
+    }
+    public Node getFilhoE() {
+        return filhoE;
+    }
+    public void setFilhoE(Node filhoE) {
+        this.filhoE = filhoE;
+    }
+    public Node getPai() {
+        return Pai;
+    }
+    public void setPai(Node pai) {
+        Pai = pai;
+    }
+
+    public int getFB() {
+        return this.FB;
+    }
+
+    public void setFB(int FB) {
+        this.FB = FB;
+    }
+    
+}
